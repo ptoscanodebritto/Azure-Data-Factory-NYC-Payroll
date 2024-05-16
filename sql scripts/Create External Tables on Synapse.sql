d412708b1fbb9047f796b9750e8a4c02b0fef016
ptoscanodebritto <paulo.toscano230382@gmail.com>
2024-05-15 21:55:19 -0400

    Add files via upload

---
+++ sql scripts/Create External Tables on Synapse.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE udacity
+
+
+USE udacity;
+
+IF NOT EXISTS (SELECT * FROM sys.external_file_formats WHERE name = 'SynapseDelimitedTextFormat') 
+	CREATE EXTERNAL FILE FORMAT [SynapseDelimitedTextFormat] 
+	WITH ( FORMAT_TYPE = DELIMITEDTEXT ,
+	       FORMAT_OPTIONS (
+			 FIELD_TERMINATOR = ',',
+			 USE_TYPE_DEFAULT = FALSE
+			))
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.external_data_sources WHERE name = 'toscanocontainer_adlsnycpayrolltoscano_dfs_core_windows_net') 
+	CREATE EXTERNAL DATA SOURCE [toscanocontainer_adlsnycpayrolltoscano_dfs_core_windows_net] 
+	WITH (
+		LOCATION = 'abfss://toscanocontainer@adlsnycpayrolltoscano.dfs.core.windows.net' 
+	)
+GO
+
+
+CREATE EXTERNAL TABLE [dbo].[NYC_Payroll_Summary](
+[FiscalYear] [int] NULL,
+[AgencyName] [varchar](50) NULL,
+[TotalPaid] [float] NULL
+)
+WITH (
+LOCATION = '/dirstaging/',
+DATA_SOURCE = [toscanocontainer_adlsnycpayrolltoscano_dfs_core_windows_net],
+FILE_FORMAT = [SynapseDelimitedTextFormat]
+)
+GO
+
+
+
+
